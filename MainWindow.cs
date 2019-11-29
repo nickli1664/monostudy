e3bbec3a71b197d0231bd9bb1e41f5eab826a51b
nickli1664 <38024846+nickli1664@users.noreply.github.com>
2019-11-29 19:32:44 +0800

    Add files via upload

--- MainWindow.cs
+++ MainWindow.cs
@@ -28,5 +28,130 @@ namespace linuxcc2
             }
         }
 
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (this.dataGridView1.SelectedRows.Count == 1)
+            {
+                Int32 drv = dataGridView1.SelectedRows[0].Index;
+                Int32 drvend = dataGridView1.Rows.Count;
+                drvend--;
+
+                if (drv < drvend)
+                {
+                    //MessageBox.Show(drv.ToString() + "----" + drvend.ToString());
+                    try
+                    {
+                        dataGridView1.Rows.RemoveAt(drv);
+                    }
+                    catch (Exception e1)
+                    {
+                        MessageBox.Show(e1.ToString());
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Don't select last row.", "Please select a row", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }     
+            }
+            else
+            {
+                MessageBox.Show("Please select one row. Don't select last row.", "Please select a row", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "Please choose path.";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                string foldPath = dialog.SelectedPath;
+                MessageBox.Show("Selected Path:" + foldPath, "Selected Path tips", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                button2.Text = foldPath; 
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "Please choose path.";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                string foldPath = dialog.SelectedPath;
+                MessageBox.Show("Selected Path:" + foldPath, "Selected Path tips", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                button3.Text = foldPath;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (button2.Text != "Click me to choose primary path" && button3.Text != "Click me to choose secondary path")
+            {
+                //MessageBox.Show("go");
+
+                int index = this.dataGridView1.Rows.Add();
+                this.dataGridView1.Rows[index].Cells[0].Value = button2.Text;
+                this.dataGridView1.Rows[index].Cells[1].Value = button3.Text;
+            }
+            else
+            {
+                MessageBox.Show("Please choose primary&secondary path.", "Please choose primary&secondary path", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int realrow = dataGridView1.RowCount-1;
+            int realcolumn = dataGridView1.ColumnCount;
+            int pairerrorflag = 0;
+
+            if (realrow == 0)
+            {
+                pairerrorflag = 1;
+                MessageBox.Show("Please add one pair at least.", "Please add pair", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                for (int i = 0; i < realrow; i++)
+                {
+                    for (int j = 0; j < realcolumn; j++)
+                    {
+                        //打印第i行第j列数据
+                        //Console.WriteLine("##" + Convert.ToString(dataGridView1.Rows[i].Cells[j].Value));
+                        string path = Convert.ToString(dataGridView1.Rows[i].Cells[j].Value);
+                        if (Directory.Exists(path))
+                        {
+                            
+                        }
+                        else
+                        {
+                            MessageBox.Show("Found error path:  " + path, "Error Path", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            pairerrorflag = 1;
+                        }
+                    }
+
+                }
+            }
+
+            if (pairerrorflag == 0 && this.button5.Text == "Lock pair")
+            {
+                this.button5.Text = "Unlock pair";
+                this.dataGridView1.ReadOnly = true;
+                this.button2.Enabled = false;
+                this.button3.Enabled = false;
+                this.button4.Enabled = false;
+                this.button6.Enabled = false;
+            }
+            else
+            {
+                this.button5.Text = "Lock pair";
+                this.dataGridView1.ReadOnly = false;
+                this.button2.Enabled = true;
+                this.button3.Enabled = true;
+                this.button4.Enabled = true;
+                this.button6.Enabled = true;
+            }
+        }
     }
 }
--- MainWindowDesign.cs
+++ MainWindowDesign.cs
@@ -34,82 +34,228 @@ namespace linuxcc2
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-
-            this.groupBox1 = new GroupBox();
-            this.label1 = new Label();
-            this.label2 = new Label();
-            this.label3 = new Label();
-            this.button1 = new Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
-
-            this.groupBox1.Location = new Point(10, 10);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(10, 10);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new Size(600, 100);
+            this.groupBox1.Size = new System.Drawing.Size(600, 100);
             this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
             this.groupBox1.Text = "First Step: Select your C&C big file";
-
+            // 
+            // label1
+            // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.label1.Location = new System.Drawing.Point(20, 25);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(104, 19);
+            this.label1.Size = new System.Drawing.Size(109, 19);
             this.label1.TabIndex = 1;
             this.label1.Text = "Big File: ";
-
+            // 
+            // label2
+            // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.label2.Location = new System.Drawing.Point(20, 60);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(104, 19);
+            this.label2.Size = new System.Drawing.Size(69, 19);
             this.label2.TabIndex = 2;
             this.label2.Text = "Size: ";
-
+            // 
+            // label3
+            // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.label3.Location = new System.Drawing.Point(105, 60);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(104, 19);
+            this.label3.Size = new System.Drawing.Size(19, 19);
             this.label3.TabIndex = 4;
             this.label3.Text = " ";
-
-            this.button1.Location = new Point(100,20);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(100, 20);
             this.button1.Name = "button1";
-            this.button1.Size = new Size(400, 30);
+            this.button1.Size = new System.Drawing.Size(400, 30);
             this.button1.TabIndex = 3;
             this.button1.Text = "Click me to choose big file";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.dataGridView1);
+            this.groupBox2.Location = new System.Drawing.Point(10, 120);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(600, 357);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Second Step: Add your C&C pair";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(317, 321);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(100, 30);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "Delete pair";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.Location = new System.Drawing.Point(6, 272);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(159, 19);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "SecondaryPath: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(6, 225);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(139, 19);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "PrimaryPath: ";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(467, 321);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 30);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Lock pair";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(167, 321);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 30);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Quick add pair";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(167, 269);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(400, 30);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Click me to choose secondary path";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(167, 222);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(400, 30);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Click me to choose primary path";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dataGridView1.Location = new System.Drawing.Point(3, 21);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(591, 186);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "PrimaryPath";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 270;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "SecondaryPath";
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 270;
+            // 
+            // lccMainWindow
+            // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 600);
-
-            this.groupBox1.Controls.Add(label1);
-            this.groupBox1.Controls.Add(label2);
-            this.groupBox1.Controls.Add(label3);
-            this.groupBox1.Controls.Add(button1);
-            this.Controls.Add(groupBox1);
-            //this.Controls.Add(label1);
-
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
-            //this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Name = "Linuxcc";
+            this.Name = "lccMainWindow";
             this.Text = "LinuxC&C";
-            //this.TopMost = true;
-            //this.Load += new System.EventHandler(this.FormEditCity_Load);
-            //this.contextMenuStrip1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
+
         }
         #endregion
 
         private Label label1;
+        private DataGridViewTextBoxColumn Column1;
+        private DataGridViewTextBoxColumn Column2;
+        private Button button2;
         private Label label2;
         private Label label3;
         private Button button1;
         private GroupBox groupBox1;
+        private DataGridView dataGridView1;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Label label4;
+        private Label label5;
+        private Button button6;
+        private GroupBox groupBox2;
+
         //private FolderBrowserDialog folderBrowserDialog1;
 
 
